c6507918a172898de315873ffea63f93acd3a060
xie pengcheng <1320401599.com>
2025-04-18 08:26:13 +0800

    s3

--- SoilSensor.py
+++ SoilSensor.py
@@ -5,14 +5,22 @@ channel = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
-def callback(channel):
-  if GPIO.input(channel):
-    print("Water not Detected!")
-  else:
-    print("Water Detected!")
+def check_sensor():
+    """检查传感器状态并打印信息"""
+    if GPIO.input(channel):
+        print("Water Detected!")
+    else:
+        print("No Water Detected!")
 
-GPIO.add_event_detect(channel,GPIO.BOTH,bouncetime=300)
-GPIO.add_event_callback(channel,callback)
+total_seconds = 4 * 24 * 60 * 60
+interval_seconds = 3 * 60 * 60
+
+start_time = time.time()
 
 while True:
-  time.sleep(0)
+    elapsed_time = time.time() - start_time
+    if elapsed_time > total_seconds:
+        break
+    if int(elapsed_time) % interval_seconds == 0:
+        check_sensor()
+    time.sleep(1)
